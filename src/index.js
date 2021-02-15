65ff00e70eddeebe9e7c14dee8445406187fb75f
Tomasz Plewka <tomasz.plewka66@gmail.com>
2021-02-15 15:36:39 -0700

    added local storage persistence using redux persist

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import App from './App';
@@ -13,13 +14,17 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
-import store from './redux/store';
+import toExport from './redux/store';
+
+const { store, persistor } = toExport;
 
 ReactDOM.render(
     <Provider store={store}>
         <React.StrictMode>
             <BrowserRouter>
-            <App />
+                <PersistGate persistor={persistor} >
+                    <App/>
+                </PersistGate>
             </BrowserRouter>
         </React.StrictMode>
     </Provider>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/user.reducer';
 import shoppingCartReducer from './shopping-cart/shopping-cart.reducer';
 
-export default combineReducers({
+const persistConfigs = {
+    key: 'root',
+    storage,
+    whitelist: ['shoppingCart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     shoppingCart: shoppingCartReducer
 });
+
+export default persistReducer(persistConfigs, rootReducer);
