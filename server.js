6db5a93e5fbbe945cd90d18bf6fcc65f2ab5a659
Tomasz Plewka <tomasz.plewka66@gmail.com>
2021-02-26 14:38:49 -0700

    updated server js

--- server.js
+++ server.js
@@ -44,9 +44,9 @@ app.post('/payment', (req, res) => {
 
     stripe.charges.create(body, (stripeErr, stripeRes) => {
         if (stripeErr) {
-        res.status(500).send({ error: stripeErr });
+            res.status(500).send({ error: stripeErr });
         } else {
-        res.status(200).send({ success: stripeRes });
+            res.status(200).send({ success: stripeRes });
         }
     });
 });
